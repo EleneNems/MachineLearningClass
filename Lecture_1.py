93a25a01a9c1c88f6347bc4895a6cb521498ef29
Y********* <y*********@gmail.com>
2025-09-23 13:48:41 +0400

    Lecture 1 commit

---
+++ Lecture_1.py
@@ -0,0 +1,36 @@
+l = [4, 8.5, "gau"]
+print(l)
+print(l[1])
+
+t = (4, 9.7, "gau")
+print(t)
+print(t[1])
+
+s = {4, 5, 6, 77, 8}
+print(s)
+s.add(53)
+print(s)
+#print(s[1])
+
+d = {}
+d['Uni'] = "Gau"
+d['Name'] = "Elene"
+print(d)
+
+list=[2, 6, 8, 4, 8]
+print(sum(list))
+print(max(list))
+print(min(list))
+print(sum(list)/len(list))
+
+list.append(102)
+print(list)
+
+list[2] = 205
+print(list)
+
+#list.remove(list[3])
+print(list)
+
+list.sort()
+print(list)
